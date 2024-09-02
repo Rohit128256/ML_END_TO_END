885a91890461ba5bb2736332fd8ef3a8418c31f5
Rohit <rohitsaha128256@gmail.com>
2024-09-03 00:06:33 +0530

    added model evaluation in utils

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 import dill
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
@@ -16,3 +17,26 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_models(x_train,y_train,x_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train,y_train)
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            model_train_score = r2_score(y_train,y_train_pred)
+
+            model_test_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = model_test_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
